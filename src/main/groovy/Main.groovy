a9f79981699d99d18cc77b8aa1c3ecefb9072cac
xiaosheng.zhang <z1178858918@qq.com>
2023-11-27 15:37:12 +0800

    commit

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,3 +1,5 @@
-static void main(String[] args) {
-  println "Hello world!"
-}
+println "hello" // a comment till the end of the line
+book='Fooled by Randomness'
+book="$book - Nassim Taleb"
+book="$book (2001)"
+assert 'Fooled by Randomness - Nassim Taleb (2001)'== book

5907c2fd269614017841bfe1056038f5f85cad37
xiaosheng.zhang <z1178858918@qq.com>
2023-12-04 13:16:18 +0800

    commit

--- src/main/groovy/ast/LogTime.groovy
+++ src/main/groovy/ast/LogTime.groovy
@@ -11,5 +11,4 @@ import java.lang.annotation.Target
 @Retention(RetentionPolicy.SOURCE)
 @Target([ElementType.METHOD])
 @GroovyASTTransformationClass(["ast.LogTimeASTTransformation"])
- @interface LogTime {
-}
+@interface LogTime {}
